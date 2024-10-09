e2f5f95f5467f49bb0f948b89abd6eaa08c514f1
aoi-sawaki <s2223210@cco.kanagawa-it.ac.jp>
2024-10-09 11:16:03 +0900

    いろいろ

---
+++ Assets/Codes/BlockManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    [SerializeField] GameObject block, flyStone;
+
+    public Vector2[] flyStonePos = new Vector2[4];  //浮遊石の座標(4つまで保存可能・(-1,-1)に設定することで無効化)
+
+    public float blockInterval = 0;   //ブロック同士の幅
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //フィールドのブロックを配置
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                Instantiate(block, new Vector3(blockInterval * i, 0, blockInterval * j), Quaternion.identity, this.transform).name = $"Block_{i}_{j}";
+            }
+        }
+
+        //フィールドに浮遊石を配置
+        for (int i = 0; i < 4; i++)
+        {
+            if (!(flyStonePos[i].x == -1 && flyStonePos[i].y == -1))
+            {
+                foreach (Transform child in this.transform)
+                {
+                    if (child.name == $"Block_{flyStonePos[i].x}_{flyStonePos[i].y}")
+                    {
+                        Destroy(child.gameObject);
+                        Instantiate(flyStone, new Vector3(blockInterval * flyStonePos[i].x, 0.5f, blockInterval * flyStonePos[i].y), Quaternion.identity, this.transform).name = $"FlyStone_{flyStonePos[i].x}_{flyStonePos[i].y}";
+                        break; // 最初に見つけたらループを抜ける
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void FallCheck()
+    {
+    }
+}
---
+++ Assets/Codes/Prefab/Block.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    bool fall;       //ブロックが落ちるフラグ
+    float fallSpeed;    //ブロックの落ちる速さ
+
+    bool countDown;     //ブロックが落ちるまでのカウントを開始するフラグ
+    float time;         //ブロックが落ちるまでの時間
+
+
+    Vector3 startPos;   //このブロックの初期位置
+    Vector3 currentPos; //このブロックの現在位置
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.position;
+
+        StateReset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (countDown == true)
+        {
+            if (time >= 0)
+            {
+                time = time - Time.deltaTime;
+            }
+            else
+            {
+                fall = true;
+            }
+        }
+        
+
+        //ブロック落下
+        if (fall == true)
+        {
+            currentPos.y -= fallSpeed * Time.deltaTime;
+            this.transform.position = currentPos;
+
+
+            if (currentPos.y < -20)
+            {
+                StateReset();
+            }
+        }
+    }
+
+    void StateReset()
+    {
+        fall = false;
+        fallSpeed = 3f;
+        countDown = false;
+        time = 5f;
+        currentPos = startPos;
+        this.transform.position = currentPos;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            countDown = true;
+        }
+    }
+}
